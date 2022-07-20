eddd3bbf2a3e732672de66cc2dc29de787ccc469
einyell <angelbelmarcmaab@gmail.com>
2022-07-20 17:50:18 +0200

    cositas en la playa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+//@ts-ignore
+import Typewriter from 't-writer.js';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'Angel Belmar';
+
+
+export class AppComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+const target1 = document.querySelector('.text1');
+const target2 = document.querySelector('.text2');
+
+const writer1 = new Typewriter(target1, {
+  typeSpeed: 40 
+})
+
+const writer2 = new Typewriter(target2, { 
+  typeSpeed: 70 
+})
+
+writer1
+  .type("I'm a ")
+  .removeCursor()
+  .then(writer2.start.bind(writer2))
+  .changeTypeColor('#F4E9DA')
+  .start()
+
+writer2
+.changeTypeColor('#85C17E')
+.type(" Front-End Developer")
+
+  .rest(700)
+  .clear()
+  .changeTypeColor('#2CABDB')
+  .type(" Software Engineer")
+  .rest(700)
+  .clear()
+  .changeTypeColor('#E7AC00')
+  .type(" Web Designer")
+  .rest(700)
+  .clear()
+  .changeTypeColor('green')
+  .then(writer1.start.bind(writer1))
+  }
+
+  
 }
